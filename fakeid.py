78ea0736ec87b0a190ccca86f30846b5595b18de
Adam Neumann <noizwaves@users.noreply.github.com>
2017-04-30 12:46:27 -0600

    Google OAuth 2.0 end points.

--- fakeid.py
+++ fakeid.py
@@ -1,7 +1,38 @@
-from flask import Flask
+from flask import Flask, jsonify, redirect, request
+from urllib.parse import urlencode
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def landing():
     return ''
+
+
+@app.route('/google/oauth2/auth')
+def google_auth():
+    uri = request.args.get('redirect_uri')
+    state = request.args.get('state')
+
+    qs = urlencode({
+        'code': 'baz',
+        'state': state
+    })
+
+    return redirect(uri + '?' + qs)
+
+
+@app.route('/google/oauth2/token', methods=['POST'])
+def google_token():
+    return jsonify({'access_token': 'waldo'})
+
+
+@app.route('/google/oauth2/info')
+def google_info():
+    return jsonify({
+        'id': 'foo-bar',
+        'displayName': 'Foo Bar',
+        'emails': [{
+            'value': 'foobar@example.com'
+        }]
+    })
--- fakeid_tests.py
+++ fakeid_tests.py
@@ -1,5 +1,7 @@
 import fakeid
 import unittest
+from json import loads
+
 
 class FakeIdTestCase(unittest.TestCase):
     def setUp(self):
@@ -8,7 +10,31 @@ class FakeIdTestCase(unittest.TestCase):
 
     def test_landing(self):
         result = self.app.get('/')
+
         assert result.status_code == 200
+
+    def test_google_auth(self):
+        result = self.app.get('/google/oauth2/auth?redirect_uri=http%3A%2F%2Fqux.com&state=garply')
+
+        assert result.status_code == 302
+        assert result.headers['Location'] == 'http://qux.com?code=baz&state=garply'
+
+    def test_google_token(self):
+        result = self.app.post('/google/oauth2/token')
+
+        assert result.content_type == 'application/json'
+        assert loads(result.data) == {'access_token': 'waldo'}
+
+    def test_google_info(self):
+        result = self.app.get('/google/oauth2/info')
+
+        assert result.status_code == 200
+        assert result.content_type == 'application/json'
+
+        body = loads(result.data)
+        assert body['id'] == 'foo-bar'
+        assert body['displayName'] == 'Foo Bar'
+        assert body['emails'][0]['value'] == 'foobar@example.com'
 
 if __name__ == '__main__':
     unittest.main()
